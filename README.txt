aa60aedbb5537948ac851bea2eb11c9f1de8aa14
Dan Tasse <dan.tasse@gmail.com>
2016-09-14 23:17:28 +0000

    first commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+Ok, take in the YFCC100M dataset, and tell how many times people toggle between geotagging and not.
---
+++ shrink_yfcc100m.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# Take in the YFCC100M file, cut out everything besides the line number, NSID
+# (user ID), date/time, and lat/lon if available.
+
+import csv, argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_file', default='yfcc1k.tsv')
+parser.add_argument('--output_file', default='small_yfcc.csv')
+args = parser.parse_args()
+
+output_writer = csv.writer(open(args.output_file, 'w'))
+output_writer.writerow(['line_number', 'nsid', 'date', 'lat', 'lon'])
+
+csv.field_size_limit(197990)
+# Got to do that or it'll choke on big fields every so often.
+# 197990 is longer than any line in this file (I checked once.)
+
+counter = 0
+for line in csv.reader(open(args.input_file), delimiter='\t'):
+    counter += 1
+    if counter % 1000000 == 0:
+        print counter
+    line_number = int(line[0])
+    nsid = line[3]
+    date = line[5]
+    lat = line[13]
+    lon = line[12]
+    output_writer.writerow([line_number, nsid, date, lat, lon])
+
+
